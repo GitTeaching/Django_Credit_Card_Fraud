aa85717632faa8d61de524ef688af88bf848fe57
ELITEBOOK G5 <mecool07.hedia@gmail.com>
2020-09-14 15:13:45 +0100

    Initial Commit

--- Django_Credit_Card_Fraud/settings.py
+++ Django_Credit_Card_Fraud/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'onwdqq*ch9c6cg1yr5deq%m81lvurjqz*3h#l_!^278==hl@73'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
